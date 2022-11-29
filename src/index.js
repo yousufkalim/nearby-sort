1d5069fdb98264145d4d2135fd1982220b36db6f
Yousuf Kalim <yousufkalim@outlook.com>
2022-11-29 18:06:17 +0500

    Created nearby sort functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
-const defaultAwesomeFunction = (name) => {
-  const returnStr = `I am the Default Awesome Function, fellow comrade! - ${name}`;
-  return returnStr;
+// Distance calculation
+const getDistance = (lat1, lon1, lat2, lon2) => {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1); // deg2rad below
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
+  return d;
 };
 
-const awesomeFunction = () => 'I am just an Awesome Function';
+// Deg conversion
+const deg2rad = (deg) => {
+  return deg * (Math.PI / 180);
+};
 
-export default defaultAwesomeFunction;
+// Sort the array according to the user's lat long
+const nearbySort = (lat, long, arr) => {
+  return new Promise((resolve, reject) => {
+    try {
+      // validateArgs(lat, long, arr);
+      const sortedArray = arr.sort((a, b) => {
+        const distanceA = getDistance(lat, long, a.lat, a.long);
+        const distanceB = getDistance(lat, long, b.lat, b.long);
+        return distanceA - distanceB;
+      });
+      resolve(sortedArray);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
-export { awesomeFunction };
+export default nearbySort;
