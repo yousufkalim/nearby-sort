1df7b1414c6b1625ce61635f934987b6114f4ecb
Yousuf Kalim <yousufkalim@outlook.com>
2022-11-29 18:07:19 +0500

    Added test cases

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,32 @@
 import { expect, test } from '@jest/globals';
-import defaultAwesomeFunction, { awesomeFunction } from '../src/index.js';
+import nearbySort from '../src/index.js';
 
-test('Should test default awesome function', () => {
-  const expectedVal = 'I am the Default Awesome Function, fellow comrade! - Yousuf';
-  expect(defaultAwesomeFunction('Yousuf')).toMatch(expectedVal);
-});
+// User's location
+const lat = 31.442907;
+const long = 74.271519;
 
-test('Should test awesome function', () => {
-  const expectedVal = 'I am just an Awesome Function';
-  expect(awesomeFunction()).toMatch(expectedVal);
+// Array of locations
+const arr = [
+  {
+    name: 'Kudos',
+    lat: 31.460443680085763,
+    long: 74.26974289747655,
+  },
+  {
+    name: 'Dr Hospital',
+    lat: 31.480616187193373,
+    long: 74.28008468009904,
+  },
+  {
+    name: 'Steak Studio',
+    lat: 31.446620698317826,
+    long: 74.2704643576623,
+  },
+];
+
+test('Should test the nearby sort function', async () => {
+  const sortedArray = await nearbySort(lat, long, arr);
+  expect(sortedArray[0].name).toBe('Steak Studio');
+  expect(sortedArray[1].name).toBe('Kudos');
+  expect(sortedArray[2].name).toBe('Dr Hospital');
 });
