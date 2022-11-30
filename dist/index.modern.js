a6d59179f0f0713e1556717b3f286cd4ab0d75bf
Yousuf Kalim <yousufkalim@outlook.com>
2022-11-30 12:50:40 +0500

    Husky chaanges

--- dist/index.modern.js
+++
@@ -1,2 +0,0 @@
-const e=e=>`I am the Default Awesome Function, fellow comrade! - ${e}`,o=()=>"I am just an Awesome Function";export{o as awesomeFunction,e as default};
-//# sourceMappingURL=index.modern.js.map
